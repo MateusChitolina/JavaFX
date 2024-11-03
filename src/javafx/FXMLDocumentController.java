114144cc301561b01358d5aeb0c493a9c2da053a
MateusChitolina <mateus.ex.chitolina@gmail.com>
2024-11-03 17:31:30 -0300

    Correção

--- src/javafx/FXMLDocumentController.java
+++ src/javafx/FXMLDocumentController.java
@@ -1,6 +1,8 @@
 
 package javafx;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
